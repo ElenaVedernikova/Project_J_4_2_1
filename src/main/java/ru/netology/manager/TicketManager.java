ae15ec774d1c5e5da283250a9c7ee8974d43c9f8
ElenaVedernikova <lenok-v@live.ru>
2021-06-29 16:29:24 +0300

    Add comparator

--- src/main/java/ru/netology/manager/TicketManager.java
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -3,9 +3,11 @@ package ru.netology.manager;
 import ru.netology.domain.Ticket;
 import ru.netology.repository.TicketRepository;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class TicketManager {
     private TicketRepository repository;
-    private String text;
 
     public TicketManager(TicketRepository repository) {
         this.repository = repository;
@@ -15,7 +17,7 @@ public class TicketManager {
         repository.save(item);
     }
 
-    public Ticket[] findAll(String from, String to) {
+    public Ticket[] findAll(String from, String to, Comparator<Ticket> comparator) {
         Ticket[] result = new Ticket[0];
         for (Ticket ticket : repository.searchBy()) {
             if (ticket.getFrom() == from && ticket.getTo() == to) {
@@ -26,6 +28,7 @@ public class TicketManager {
                 result = tmp;
             }
         }
+        Arrays.sort(result, comparator);
         return result;
     }
 }
